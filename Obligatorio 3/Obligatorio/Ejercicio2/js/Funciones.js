2c9a0acae519dccdc1e0eba77947b61acf6f6943
Oth1 <gonzalo.otheguy@hotmail.com>
2018-03-06 01:36:53 -0300

    s

--- Obligatorio 3/Obligatorio/Ejercicio2/js/Funciones.js
+++ Obligatorio 3/Obligatorio/Ejercicio2/js/Funciones.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).ready(function(){
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 50) {
+               $('#back-to-top').fadeIn();
+           } else {
+               $('#back-to-top').fadeOut();
+           }
+       });
+       // scroll body to 0px on click
+       $('#back-to-top').click(function () {
+           $('#back-to-top').tooltip('hide');
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+       
+       $('#back-to-top').tooltip('show');
+
+});
+
+$("input.form-control-search").click(function () {
+    $(this).addClass("changeStyle");
+});
+
+$(document).ready(function(){
+    $(".start-slide").click(function(){
+        $("#myCarousel").carousel('cycle');
+    });
+});
+
+$('.carousel').carousel({
+    interval: 1000
+  })
+
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
